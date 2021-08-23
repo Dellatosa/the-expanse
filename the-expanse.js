da29b293cee9aa4b941cd0c4c99b4147473b7096
Dellatosa <68203012+Dellatosa@users.noreply.github.com>
2021-08-23 16:50:39 +0200

    upload 1

---
+++ the-expanse.js
@@ -0,0 +1,370 @@
+// import * as Macros from "./modules/macros.js";
+import {ageSystem} from "./modules/config.js";
+import ageSystemSheetItem from "./modules/sheets/ageSystemSheetItem.js";
+import ageSystemSheetCharacter from "./modules/sheets/ageSystemSheetCharacter.js";
+import ageSystemSheetCharStatBlock from "./modules/sheets/ageSystemSheetCharStatBlock.js";
+import ageSystemSheetVehicle from "./modules/sheets/ageSystemSheetVehicle.js";
+import ageSystemSheetSpaceship from "./modules/sheets/ageSystemSheetSpaceship.js";
+import ageActiveEffectConfig from "./modules/sheets/ageActiveEffectConfig.js";
+import {ageSystemActor} from "./modules/ageSystemActor.js";
+import {ageEffect} from "./modules/ageEffect.js";
+import {ageToken} from "./modules/ageToken.js";
+import {ageSystemItem} from "./modules/ageSystemItem.js";
+import { createAgeMacro } from "./modules/macros.js";
+import { rollOwnedItem } from "./modules/macros.js";
+import { AgeRoller } from "./modules/age-roller.js";
+import { AgeTracker } from "./modules/age-tracker.js";
+
+import * as Dice from "./modules/dice.js";
+import * as Settings from "./modules/settings.js";
+import * as AgeChat from "./modules/age-chat.js";
+import * as Setup from "./modules/setup.js";
+import * as migrations from "./modules/migration.js";
+
+async function preloadHandlebarsTemplates() {
+    const path = `systems/the-expanse/templates/partials/`;
+    const templatePaths = [
+        `${path}bonus-desc-sheet.hbs`,
+        `${path}dmg-block-sheet.hbs`,
+        `${path}bonuses-sheet.hbs`,
+        `${path}active-bonuses.hbs`,
+        `${path}ability-focus-select.hbs`,
+        `${path}cost-resource-block.hbs`,
+        `${path}play-aid-bar.hbs`,
+        `${path}item-image-sheet-card.hbs`,
+        `${path}conditions-block.hbs`,
+        `${path}char-sheet-nav-bar.hbs`,
+        `${path}char-sheet-tab-main.hbs`,
+        `${path}char-sheet-tab-persona.hbs`,
+        `${path}char-sheet-tab-effects.hbs`,
+        `${path}item-card-buttons.hbs`,
+        `${path}char-stat-block-column1.hbs`,
+    ];
+
+    return loadTemplates(templatePaths);
+};
+
+Hooks.once("init", async function() {
+    const ageSystemText = `
+     ___   ____________   _____            __               
+    /   | / ____/ ____/  / ___/__  _______/ /____  ____ ___ 
+   / /| |/ / __/ __/     \\__ \\/ / / / ___/ __/ _ \\/ __ \`__ \\
+  / ___ / /_/ / /___    ___/ / /_/ (__  ) /_/  __/ / / / / /
+ /_/  |_\\____/_____/   /____/\\__, /____/\\__/\\___/_/ /_/ /_/ 
+                            /____/                          `;
+
+    console.log("The Expanse | Entering a new AGE...");
+    console.log(ageSystemText);
+
+    // Create a namespace within the game global
+    game.ageSystem = {
+        applications: {
+            ageSystemSheetCharacter,
+            ageSystemSheetCharStatBlock,
+            ageSystemSheetVehicle,
+            ageSystemSheetSpaceship,
+            // ageActiveEffectConfig,
+            ageSystemSheetItem,
+            AgeRoller,
+            AgeTracker
+        },
+        dice: Dice,
+        migrations: migrations,
+        rollOwnedItem,
+        entities: {
+            ageSystemActor,
+            ageToken,
+            ageSystemItem,
+            ageEffect
+        }
+    };
+
+    CONFIG.ageSystem = ageSystem;
+
+    // Define Token Icons
+    CONFIG.statusEffects = ageSystem.AGEstatusEffects;
+    // Changing a few control icons
+    CONFIG.controlIcons.defeated = "systems/the-expanse/resources/imgs/effects/hasty-grave.svg"
+
+    Actors.unregisterSheet("core", ActorSheet);
+    Actors.registerSheet("the-expanse", ageSystemSheetCharacter, {
+        types: ["char"],
+        makeDefault: true,
+        label: "the-expanse.SHEETS.charStandard"
+    });
+    Actors.registerSheet("the-expanse", ageSystemSheetCharStatBlock, {
+        types: ["char"],
+        label: "the-expanse.SHEETS.charStatBlock"
+    });
+    Actors.registerSheet("the-expanse", ageSystemSheetVehicle, {
+        types: ["vehicle"],
+        makeDefault: true,
+        label: "the-expanse.SHEETS.vehicleStandard"
+    });
+    Actors.registerSheet("the-expanse", ageSystemSheetSpaceship, {
+        types: ["spaceship"],
+        makeDefault: true,
+        label: "the-expanse.SHEETS.spaceshipStandard"
+    });
+    
+    Items.unregisterSheet("core", ItemSheet);
+    Items.registerSheet("the-expanse", ageSystemSheetItem, {
+        makeDefault: true,
+        label: "the-expanse.SHEETS.itemStandard"
+    });
+
+    game.ageSystem.ageRoller = new AgeRoller({
+        popOut: false,
+        minimizable: false,
+        resizable: false,
+    });
+
+    game.ageSystem.ageTracker = new AgeTracker({
+        popOut: false,
+        minimizable: false,
+        resizable: false
+    });
+
+    // Define extra data for Age System (Actors, Items, ActiveEffectConfig)
+    CONFIG.Actor.documentClass = ageSystemActor;
+    CONFIG.Token.objectClass = ageToken;
+    CONFIG.Item.documentClass = ageSystemItem;
+    CONFIG.ActiveEffect.documentClass = ageEffect;
+    // Saving this customization for a later implementation
+    // CONFIG.ActiveEffect.sheetClass = ageActiveEffectConfig;
+
+    // Load partials for Handlebars
+    preloadHandlebarsTemplates();
+
+    // Register System Settings
+    Settings.registerSystemSettings();
+
+    // Useful concat Helper from Boilerplate system!
+    Handlebars.registerHelper('concat', function() {
+        let outStr = '';
+        for (let arg in arguments) {
+            if (typeof arguments[arg] != 'object') {
+                outStr += arguments[arg];
+            }
+        }
+        return outStr;
+    });
+
+    // Handlebar to identify type of Effect
+    Handlebars.registerHelper('ageffect', function(mask, options) {
+        for (let o = 0; o < options.length; o++) {
+            const e = options[o];
+            if (e[1] === mask) return e[0]
+        }
+        return `${mask} (${game.i18n.localize("the-expanse.custom")})`;
+    })
+
+    // Handlebar returning array with Focus for a given Ability
+    Handlebars.registerHelper('focusbyabl', function(focus, abl) {
+        return focus.filter(f => f.data.useAbl === abl)
+    })
+
+    // Handlebar returning array with equiped weapon
+    Handlebars.registerHelper('equippedwpn', function(weapons) {
+        return weapons.filter(f => f.data.equiped === true)
+    })
+
+    // Handlebar returning array with active Power dealing damage
+    Handlebars.registerHelper('dmgpower', function(powers) {
+        return powers.filter(p => p.data.activate === true && (p.data.hasDamage || p.data.hasHealing))
+    })
+
+    // Handlebar returning all carried itens
+    Handlebars.registerHelper('carriedequip', function(items) {
+        return items.filter(p => p.type === "equipment" || p.type === "weapon")
+    })
+
+    // Handlebar helper to compare 2 data
+    Handlebars.registerHelper("when",function(operand_1, operator, operand_2, options) {
+        let operators = {
+            'eq': function(l,r) { return l == r; },
+            'noteq': function(l,r) { return l != r; },
+            'gt': function(l,r) { return Number(l) > Number(r); },
+            'or': function(l,r) { return l || r; },
+            'and': function(l,r) { return l && r; },
+            '%': function(l,r) { return (l % r) === 0; }
+        },
+        result = operators[operator](operand_1,operand_2);
+      
+        if (result) return options.fn(this);
+        else return options.inverse(this);
+    });
+
+    // Keep a list of actors that need to prepareData after 'ready' (generally those that rely on other actor data - passengers/mounts)
+    game.postReadyPrepare = [];
+
+});
+
+Hooks.once("setup", function() {
+    // Localize conditions
+    for (let c = 0; c < ageSystem.conditions.length; c++) {
+        const cond = ageSystem.conditions[c];
+        ageSystem.conditions[c].name = game.i18n.localize(ageSystem.conditions[c].name);
+        ageSystem.conditions[c].desc = game.i18n.localize(ageSystem.conditions[c].desc);
+    }
+
+    // Localize Abilities' name
+    Setup.abilitiesName();
+
+    // Localize Effects Name and build object
+    Setup.localizeAgeEffects();
+
+    // Localize Item modifier label
+    // Setup.localizeModifiers();
+
+});
+
+Hooks.once("ready", async function() {
+    // Identify Colorset
+    const color = game.user.getFlag("the-expanse", "colorScheme");
+    if (color) game.settings.set("the-expanse", "colorScheme", color);
+    if (!color) game.user.setFlag("the-expanse", "colorScheme", game.settings.get("the-expanse", "colorScheme"));
+    // Register color scheme on global name space
+    ageSystem.colorScheme = game.settings.get("the-expanse", "colorScheme");
+
+    // Tracker Handling
+    // Identify if User already has ageTrackerPos flag set
+    const userTrackerFlag = await game.user.getFlag("the-expanse", "ageTrackerPos");
+    const useTracker = (game.settings.get("the-expanse", "serendipity") || game.settings.get("the-expanse", "complication") !== "none") ? true : false;
+    if (!userTrackerFlag) await game.user.setFlag("the-expanse", "ageTrackerPos", ageSystem.ageTrackerPos);
+    if (useTracker) game.ageSystem.ageTracker.refresh();
+
+    // Age Roller
+    // Handle flag
+    const rollerFlag = await game.user.getFlag("the-expanse", "ageRollerPos");
+    if (!rollerFlag) await game.user.setFlag("the-expanse", "ageRollerPos", ageSystem.ageRollerPos);
+    game.ageSystem.ageRoller.refresh()
+
+    // Check if Dice so Nice is active to register Stunt Die option
+    if (game.modules.get("dice-so-nice") && game.modules.get("dice-so-nice").active) {
+        import("/modules/dice-so-nice/DiceColors.js").then((diceColors) => {
+            
+            const colorset = diceColors.COLORSETS;
+            let colorChoices = {};
+            for (const type in colorset) {
+                if (colorset.hasOwnProperty(type)) {
+                    const colorCode = colorset[type].name;
+                    const colorName = colorset[type].description;
+                    const newChoice = {[colorCode]: colorName}
+                    colorChoices = {
+                    ...colorChoices,
+                    ...newChoice
+                    };
+                };
+            };
+            if (colorChoices !== {} && colorChoices) {
+                // After loading all modules, check if Dice so Nice is installed and add option to select Stunt Die colorset                
+                Settings.stuntSoNice(colorChoices);
+                // Identify if user has registered Dice so Nice Stunt Die option
+                const stuntSoNiceFlag = game.user.getFlag("the-expanse", "stuntSoNice");
+                if (stuntSoNiceFlag) game.settings.set("the-expanse", "stuntSoNice", stuntSoNiceFlag);
+                if (!stuntSoNiceFlag) game.user.setFlag("the-expanse", "stuntSoNice", game.settings.get("the-expanse", "stuntSoNice"));
+            };
+        });
+    };
+
+    // Prepare Actors dependent on other Actors
+    for(let e of game.postReadyPrepare){
+        e.prepareData();
+    }
+    
+    // Register System Settings related do Focus Compendium
+    ageSystem.itemCompendia = Settings.allCompendia("Item");
+    Settings.loadCompendiaSettings();
+    const setCompendium = game.settings.get("the-expanse", "masterFocusCompendium");
+    ageSystem.focus = Settings.compendiumList(setCompendium);
+
+    // Wait to register hotbar drop hook on ready so that modules could register earlier if they want to
+    Hooks.on("hotbarDrop", (bar, data, slot) => createAgeMacro(data, slot));
+
+    // // Determine whether a system migration is required and feasible
+    if ( !game.user.isGM ) return;
+    const currentVersion = game.settings.get("the-expanse", "systemMigrationVersion");
+    const NEEDS_MIGRATION_VERSION = "0.0.0";
+    // const COMPATIBLE_MIGRATION_VERSION = "0.8.7";
+    const needsMigration = !currentVersion || isNewerVersion(NEEDS_MIGRATION_VERSION, currentVersion);
+    if ( !needsMigration ) return;
+
+    // Perform the migration
+    // if ( currentVersion && isNewerVersion(COMPATIBLE_MIGRATION_VERSION, currentVersion) ) {
+    //     const warning = `Your The Expanse System data is from too old a Foundry version and cannot be reliably migrated to the latest version. The process will be attempted, but errors may occur.`;
+    //     ui.notifications.error(warning, {permanent: true});
+    // }
+    migrations.migrateWorld();
+
+});
+
+Hooks.on("createCompendium", () => {
+    ageSystem.itemCompendia = Settings.allCompendia("Item");
+})
+
+// If Compendia are updated, then compendiumList is gathered once again
+Hooks.on("renderCompendium", () => {
+    const setCompendium = game.settings.get("the-expanse", "masterFocusCompendium");
+    ageSystem.focus = Settings.compendiumList(setCompendium);
+});
+
+Hooks.on("renderageSystemItemSheet", (app, html, data) => {
+    // Add item type on title bar
+    Setup.nameItemSheetWindow(app);
+});
+
+Hooks.on("renderageSystemSheetCharacter", (app, html, data) => {
+    // Hide primary Abilities checkbox
+    Setup.hidePrimaryAblCheckbox(html);
+});
+
+Hooks.on("renderChatLog", (app, html, data) => AgeChat.addChatListeners(html));
+
+Hooks.on("renderChatMessage", (app, html, data) => {
+    // Hide chat message when rolling to GM
+    AgeChat.sortCustomAgeChatCards(app, html, data);
+});
+
+// Prevent Items to be created on non campatible Actor types
+Hooks.on("preCreateItem", (itemCreated, itemCreatedData, options, userId) => {
+    // Ensure this change occurs once
+    if (game.user.id !== userId) return;
+
+    const actor = itemCreated.actor;
+    const itemName = itemCreatedData.name
+    const itemType = itemCreatedData.type
+
+    if (!actor) return;
+
+    // Avoid dropping Item on Vehicle
+    if (actor.type === "vehicle") {
+        ui.notifications.warn(`Items can not be droped on Vehicle Actor type.`);
+        return options.temporary = true;
+    };
+
+    // Ensure only Spaceship Features are droped on Spaceships
+    if (actor.type === "spaceship" && itemType !== "shipfeatures") {
+        let warning = game.i18n.localize("the-expanse.WARNING.nonShipPartsOnShip");
+        ui.notifications.warn(warning);
+        return options.temporary = true;
+    }
+    
+    // Prevent adding spaceship features to Actors
+    if (actor.type === "char" && itemType === "shipfeatures") {
+        let warning = game.i18n.localize("the-expanse.WARNING.shipPartsOnChar");
+        ui.notifications.warn(warning);
+        return options.temporary = true;
+    }
+    
+    // Avoid Focus with repeated name on Actors
+    if (actor.type === "char" && itemType === "focus") {
+        const hasFocus = actor.items.filter(f => f.name === itemCreatedData.name && f.type === "focus");
+        if (hasFocus.length > 0) {
+            let warning = game.i18n.localize("the-expanse.WARNING.duplicatedFocus");
+            warning += `"${itemName.toUpperCase()}"`;
+            ui.notifications.warn(warning);
+            return options.temporary = true;
+        }
+    }
+});
